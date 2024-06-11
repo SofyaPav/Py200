97745b79f101915b5a4bc5fc0036b16ee662f3dc
sofpav <sofyapavlova12@gmail.com>
2024-06-11 19:32:39 +0300

    1st commit

---
+++ Основы ООП/Практические задания/task1_Glass__init__/main.py
@@ -0,0 +1,13 @@
+from typing import Union  # Аннотация типов в функции
+
+
+class Glass:
+    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        self.capacity_volume = 300
+        self.occupied_volume = 100
+
+
+if __name__ == "__main__":
+    ...  # TODO инициализировать два объекта типа Glass
+
+    # TODO попробовать инициализировать не корректные объекты

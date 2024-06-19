ff7d8b2a8fdf0362a3395c207f406f1659b7236d
Sofya <sofyapavlova12@gmail.com>
2024-06-19 20:42:05 +0300

    2024.06.19_20-40_создала в "Атрибуты и методы"-"task4" файлы help.py и theory.py

--- Атрибуты и методы/Практические задания/task4_Glass__class__name__/help.py
+++ Атрибуты и методы/Практические задания/task4_Glass__class__name__/help.py
@@ -1,13 +1,13 @@
-from typing import Union
+
 
 
 class Glass:
 
-    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+    def __init__(self, capacity_volume, occupied_volume):
         self.capacity_volume = capacity_volume  # объем стакана
         self.occupied_volume = occupied_volume  # объем жидкости в стакане
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         # self.__class__.__name__ вместо явного указания названия класса
         return f"{self.__class__.__name__}({self.capacity_volume}, {self.occupied_volume})"
 

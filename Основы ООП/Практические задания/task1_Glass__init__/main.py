3db4e0d07108074f1fd935d3a4f4031e66286fa1
Sofya <sofyapavlova12@gmail.com>
2024-06-19 18:35:47 +0300

    1

--- Основы ООП/Практические задания/task1_Glass__init__/main.py
+++ Основы ООП/Практические задания/task1_Glass__init__/main.py
@@ -1,6 +1,5 @@
 from typing import Union  # Аннотация типов в функции
-
-
+кеку
 class Glass:
     def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
         self.capacity_volume = 300

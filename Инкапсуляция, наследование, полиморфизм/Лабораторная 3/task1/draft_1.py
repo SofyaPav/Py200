37ee92d88f845ccdf7cf31f62b532dfb8f1ac165
SofyaPav <sofyapavlova12@gmail.com>
2025-02-22 00:38:19 +0300

    лаб3 сделана

--- Инкапсуляция, наследование, полиморфизм/Лабораторная 3/task1/draft_1.py
+++ Инкапсуляция, наследование, полиморфизм/Лабораторная 3/task1/draft_1.py
@@ -1,6 +1,3 @@
-
-
-
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
@@ -9,44 +6,64 @@ class Book:
 
     @property
     def name(self):
-        return self.name
+        return self._name
 
     @property
     def author(self):
-        return self.author
+        return self._author
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self._name}. Автор {self._author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        self.pages = None
-        self.check_paper(pages)
+        self._pages = pages
 
-    def check_paper(self, value_paper):
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value_paper):
         if not isinstance(value_paper, int):
-            raise TypeError
-        self.pages = value_paper
+            raise TypeError("Количество страниц должно быть целым числом")
+        if value_paper <= 0:
+            raise ValueError("Количество страниц должно быть больше 0")
+        self._pages = value_paper
+
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Бумажная книга {self._name}. Автор {self._author}. Количество страниц {self._pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self._pages!r})"
 
 
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        self.duration = None
-        self.check_audio(duration)
+        self._duration = duration
 
-    def check_audio(self, value_audio):
-        if not isinstance(value_audio, float):
-            raise TypeError
-        self.duration = value_audio
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value_duration):
+        if not isinstance(value_duration, float):
+            raise TypeError("Продолжительность должна быть числом с плавающей запятой")
+        if value_duration <= 0:
+            raise ValueError("Продолжительность должна быть больше 0")
+        self._duration = value_duration
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Аудиокнига {self._name}. Автор {self._author}. Продолжительность {self._duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self._duration!r})"
+
--- Инкапсуляция, наследование, полиморфизм/Лабораторная 3/task1/main.py
+++ Инкапсуляция, наследование, полиморфизм/Лабораторная 3/task1/main.py
@@ -1,33 +1,69 @@
-
-
 class Book:
     """ Базовый класс книги. """
+
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self._name}. Автор {self._author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name, author)
+        self._pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, value_paper):
+        if not isinstance(value_paper, int):
+            raise TypeError("Количество страниц должно быть целым числом")
+        if value_paper <= 0:
+            raise ValueError("Количество страниц должно быть больше 0")
+        self._pages = value_paper
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Бумажная книга {self._name}. Автор {self._author}. Количество страниц {self._pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self._pages!r})"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self._duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, value_duration):
+        if not isinstance(value_duration, float):
+            raise TypeError("Продолжительность должна быть числом с плавающей запятой")
+        if value_duration <= 0:
+            raise ValueError("Продолжительность должна быть больше 0")
+        self._duration = value_duration
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Аудиокнига {self._name}. Автор {self._author}. Продолжительность {self._duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self._duration!r})"
+
